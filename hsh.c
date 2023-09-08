39807c60e394bcc5d3a5d2025d3a52cf010d4071
amir-kedis <amir.kedis@gmail.com>
2023-09-08 21:01:05 +0300

    ✨ Make the shell loop infinite

--- hsh.c
+++ hsh.c
@@ -7,15 +7,28 @@
 void hsh(void)
 {
 	/* TODO: implement and adjust prototype as appropriate */
+	/* this is an ecpermiental function */
+	/* that prints the prompt and prints back the tokens that is gets */
 	char *line;
 	size_t len = 0;
 	ssize_t read_count;
+	char *word;
 
 	do
 	{
 		_puts(PROMPT);
 		read_count = getline(&line, &len, stdin);
-		_puts(line);
+
+		if (read_count != -1)
+		{
+			word = strtok(line, DILIM);
+			while (word != NULL)
+			{
+				_puts(word);
+				_puts("\n");
+				word = strtok(NULL, DILIM);
+			}
+		}
 	}
 	while (read_count != -1);
 
