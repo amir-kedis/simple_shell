f344ddc76e3506a91f5aaaa731a8b2bab45315ba
amir-kedis <amir.kedis@gmail.com>
2023-09-08 15:01:49 +0300

    ✨ implement simple print functinos with buffer handling

--- hsh.c
+++ hsh.c
@@ -9,10 +9,15 @@ void hsh(void)
 	/* TODO: implement and adjust prototype as appropriate */
 	char *line;
 	size_t len = 0;
+	ssize_t read_count;
 
-	printf("%s", PROMPT);
-	getline(&line, &len, stdin);
-	printf("%s", line);
+	do
+	{
+		_puts(PROMPT);
+		read_count = getline(&line, &len, stdin);
+		_puts(line);
+	}
+	while (read_count != -1);
 
 	free(line);
 	return;
