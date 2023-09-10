1d400ad8bf2be1bd4b1747248dadb7be6b3fc4ba
amir-kedis <amir.kedis@gmail.com>
2023-09-10 13:07:51 +0300

    ✏ Add more strings functions

--- string_lib_1.c
+++ string_lib_1.c
@@ -17,3 +17,55 @@ void _strcpy(char *src, char *ogdest)
 	}
 	*dest = '\0';
 }
+
+/**
+ * copy_str_array - copy a string array
+ * @str_array: string array to copy
+ * Return: pointer to copy
+ */
+char **copy_str_array(char **str_array)
+{
+	int i, len;
+	char **copy;
+
+	len = 0;
+	while (str_array[len] != NULL)
+		len++;
+
+	copy = malloc(sizeof(char *) * (len + 1));
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+	{
+		copy[i] = malloc(sizeof(char) * (_strlen(str_array[i]) + 1));
+		if (copy[i] == NULL)
+		{
+			free_str_array(copy);
+			return (NULL);
+		}
+		_strcpy(str_array[i], copy[i]);
+	}
+	copy[i] = NULL;
+
+	return (copy);
+}
+
+/**
+ * free_str_array - free a string array
+ * @str_array: string array to free
+ * Return: void
+ */
+
+void free_str_array(char **str_array)
+{
+	int i;
+
+	if (str_array == NULL)
+		return;
+
+	for (i = 0; str_array[i] != NULL; i++)
+		free(str_array[i]);
+
+	free(str_array);
+}
